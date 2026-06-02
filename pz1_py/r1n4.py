35c2fe9697ec1e67d7ce5a6534bd25163c4a143e
frien9zone <163419929+frien9zone@users.noreply.github.com>
2026-06-02 15:39:30 +0200

    Add files via upload

---
+++ pz1_py/r1n4.py
@@ -0,0 +1,4 @@
+def smallest_number(a, b, c):
+    print("Найменше число:", min(a, b, c))
+    
+smallest_number(5, 2, 9)
---
+++ pz1_py/r2n4.py
@@ -0,0 +1,5 @@
+def reverse_string(text):
+    return text[::-1]
+
+result = reverse_string("hello")
+print("Обернений рядок:", result)
